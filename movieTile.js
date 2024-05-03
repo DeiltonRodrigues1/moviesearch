3af9fa9307c822b1cab7ede277fc22e600243a0f
Deilton Rodrigues <97299401+DeiltonRodrigues1@users.noreply.github.com>
2024-05-03 17:27:20 -0300

    Add files via upload

---
+++ movieTile.js
@@ -0,0 +1,25 @@
+import { LightningElement, api } from 'lwc';
+
+export default class MovieTile extends LightningElement {
+    @api movie;
+    @api selectedMovieId;
+
+    clickHandler(event){
+        console.log(this.movie.imdbID);
+       //customEvents 
+       //1. create the event
+       const evt = new CustomEvent("selectedmovie",{
+        detail : this.movie.imdbID
+       });
+
+       //2. dispatch event
+
+       this.dispatchEvent(evt);
+    }
+
+    get tileSelected(){
+        return this.selectedMovieId === this.movie.imdbID 
+        ? "tile selected" 
+        : "tile";
+    }
+}
